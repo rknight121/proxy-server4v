add76dcd7faa97175967a905b171c95268e4ecaa
rknight121 <richardknight98@gmail.com>
2025-02-26 15:09:45 -0600

    first

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "aviation-weather-proxy",
+  "version": "1.0.0",
+  "description": "Proxy server for NOAA Aviation Weather API",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "cors": "^2.8.5",
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
